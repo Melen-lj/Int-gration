f7dc321bf22efb6b6968851ead1df297c161a8f9
Melen-lj <lejeune.m@stjolorient.fr>
2025-01-13 11:55:58 +0100

    Modif de diff et same

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,11 +1,13 @@
 """Module manipulant des fichiers texte."""
+
 def diff(file_first, file_second):
+	"""Fonction retournant True si deux fichiers sont différents."""
 	resultat=False
-	with open(file_first) as file1_id:
-		with open(file_second) as file2_id:
+	with open(file_first, encoding="utf-8") as file1_id:
+		with open(file_second, encoding="utf-8") as file2_id:
 			resultat=file1_id.read()!=file2_id.read()
 	return resultat
 
 def same(file_first, file_second):
     """Fonction retournant True si deux fichiers sont identiques."""
-    return True
+    return not diff(file_first, file_second)
