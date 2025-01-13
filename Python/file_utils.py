4adb1259ff95d5ed1c21f4e24a55efe79772d01b
Melen-lj <lejeune.m@stjolorient.fr>
2025-01-13 11:43:30 +0100

    modif diff

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,7 +1,7 @@
 """Module manipulant des fichiers texte."""
 def diff(file_first, file_second):
-    """Fonction retournant True si deux fichiers sont différents."""
-	resultat=False
+	"""Fonction retournant True si deux fichiers sont différents."""
+	resultat = False
 	with open(file_first) as file1_id:
 		with open(file_second) as file2_id:
 			resultat= file1.id.read() != file2_id.read()
