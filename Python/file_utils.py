0d728abf721742d94343e50f373cc522d6afb68f
Melen-lj <lejeune.m@stjolorient.fr>
2025-01-13 11:25:01 +0100

    Modif diff

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,8 +1,12 @@
 """Module manipulant des fichiers texte."""
-
+from filecmp import dircmp
 def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
-    return True
+    resultat=False
+    with open(file_first) as file1_id:
+		with open(file_second) as file2_id:
+			resultat= file1.id.read()!=file_id.read()
+	return resultat
 
 def same(file_first, file_second):
     """Fonction retournant True si deux fichiers sont identiques."""
